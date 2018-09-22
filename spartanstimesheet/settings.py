2e296db8db7d3f6a63594a4286aa8e3ae7613975
pabloferr92 <pabloferr92@gmail.com>
2018-09-22 14:35:35 -0300

    Adicionada conexão com PostgreSQL

--- spartanstimesheet/settings.py
+++ spartanstimesheet/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'spartanstimesheet.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'timesheet',
+        # 'NAME': os.path.join(BASE_DIR, 'mydb'),
+        'USER': 'postgres',
+        'PASSWORD': 'abc321',
+        'HOST': '127.0.0.1',
+        'PORT': '', # 8000 is default
     }
 }
 
